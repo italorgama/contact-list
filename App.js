343ff35c9f723e77003769dc69c7ee683ab2c93f
Ítalo Gama <itrgama@gmail.com>
2018-07-22 23:29:29 -0300

    Criando a navegação do aplicativo.

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
-
+import React from 'react';
+import { createStackNavigator } from 'react-navigation'; 
 import PeoplePage from './src/pages/PeoplePage';
 
-
-export default PeoplePage;
+export default class App extends React.Component {
+    render() {
+      return <MyNavigator />;
+    }
+  }
+const MyNavigator = createStackNavigator({
+    'Main': PeoplePage,
+});
