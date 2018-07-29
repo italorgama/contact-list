4849cbe1ae7daa3c7e44a75d36df05aa8e257d2f
Ítalo Gama <itrgama@gmail.com>
2018-07-28 22:44:01 -0300

    Finalizado meu primeiro aplicativo em ReactNative.
    Aplicativo bem simples, porém serviu para aprendizado de diversos conceitos utilizados em ReactNative. Em breve vou começar um projeto mais complexo em ReactNative.

--- src/pages/PeoplePage.js
+++ src/pages/PeoplePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 import PeopleList from '../components/PeopleList';
 import axios from 'axios';
 
@@ -23,31 +23,59 @@ export default class PeoplePage extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-        peoples: []
+        peoples: [],
+        loading: false,
+        error: false,
     };
   }
 
   componentDidMount(){ 
-    axios
-        .get('https://randomuser.me/api/?nat=BR&results=150')
-        .then(response => {
-          const { results } = response.data;      
-          this.setState({
-            peoples: results
-          });   
-        })        
-  }
+    this.setState({ loading: true});
+    setTimeout(() => {
+        axios
+            .get('https://randomuser.me/api/?nat=BR&results=15')
+            .then(response => {
+                const { results } = response.data;      
+                this.setState({
+                  peoples: results,
+                  loading: false,
+                });   
+            }).catch(error => {
+                this.setState({ 
+                  loading: false,
+                  error: true,
+                 })
+            }); 
+    }, 3500)          
+  } 
 
-  render() {
-    //this.props.navigation.navigate('PeopleDetail');
+  render() {    
     return (
-      <View>
-          <PeopleList 
-              peoples={this.state.peoples}
-              onPressItem={pageParams => {
-                this.props.navigation.navigate('PeopleDetail', pageParams);
-              }}/>       
+      <View style={styles.container}>
+          {
+             this.state.loading 
+                ? <ActivityIndicator size="large" color="#6ca2f7" />
+                : this.state.error
+                    ? <Text style={styles.error}>Ops... Algo deu errado :'( </Text>
+                    : <PeopleList 
+                          peoples={this.state.peoples}
+                          onPressItem={pageParams => {
+                            this.props.navigation.navigate('PeopleDetail', pageParams);
+                          }}/>
+          }                 
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      justifyContent: 'center',
+  },
+  error: {
+    color:'red',
+    alignSelf: 'center',
+    fontSize: 18,
+  }
+});
